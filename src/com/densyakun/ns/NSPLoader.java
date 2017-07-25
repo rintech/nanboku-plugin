8cb4f5ca02dc73d5580209690b268f772b0f9f6b
Ryo Nakagome <nakagome@tento-net.com>
2017-07-25 18:07:03 +0900

    トロッコ系のクラス作成

--- src/com/densyakun/ns/NSPLoader.java
+++ src/com/densyakun/ns/NSPLoader.java
@@ -1,7 +1,34 @@
 package com.densyakun.ns;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NSPLoader extends JavaPlugin {
+public class NSPLoader extends JavaPlugin implements Listener {
+
+	//初期動作
+	public void onEnable() {
+		//This Event
+		getServer().getPluginManager().registerEvents(this, this);
+
+		//Vehicle Control
+		getServer().getPluginManager().registerEvents(new VehicleControl(), this);
+		getLogger().info("有効になりました");
+	}
+
+	public void onDisable() {
+
+	}
+
+	/* @EventHandler
+	public void onPlayerJoin(PlayerJoinEvent je) {
+		Server server = getServer();
+		Player player = je.getPlayer();
+		je.getPlayer().sendMessage(ChatColor.AQUA + "ようこそ" + player.getDisplayName() + "さん！");
+		server.broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ChatColor.AQUA + "さんがオンラインになりました。");
+		server.broadcastMessage(ChatColor.GOLD + "みなさんで歓迎しましょう！");
+	}
+
+	*/
+
 
 }
---
+++ src/com/densyakun/ns/VehicleControl.java
@@ -0,0 +1,107 @@
+package com.densyakun.ns;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.util.Vector;
+
+public class VehicleControl implements Listener {
+
+	// トロッコ乗車時処理
+	@EventHandler
+	public void onEnterVehicle(VehicleEnterEvent ve) {
+		if (ve.getVehicle().getType() == EntityType.MINECART && ve.getEntered() instanceof Player) {
+			Player player = (Player) ve.getEntered();
+			player.sendMessage(ChatColor.GOLD + "手動運転の方法");
+			player.sendMessage("左クリック：ブレーキ(押しっぱなし)");
+			player.sendMessage("右クリック：加速(押しっぱなしかつ移動中のみ)");
+			player.sendMessage(ChatColor.GREEN + "TrainCarts適用区間及び自動運転区間ではこの限りではありません。");
+		}
+	}
+
+	@EventHandler
+	public void onBlockPlaceWhenRiding(BlockPlaceEvent pe) {
+		if(pe.getPlayer() != null) {
+			if(pe.getPlayer().getVehicle() instanceof Minecart) {
+				pe.setCancelled(true);
+			}
+		} else {
+			pe.setCancelled(true);
+		}
+	}
+
+	// トロッコ（手動運転）
+	@EventHandler
+	public void onClickBlock(PlayerInteractEvent ie) {
+		if((ie.getPlayer().getVehicle() != null) && (ie.getPlayer().getVehicle() instanceof Player)) {
+			Vector vector = ie.getPlayer().getVehicle().getVelocity();
+			switch (ie.getAction()) {
+			//トロッコ乗車中に左クリック（ブレーキ）
+			case LEFT_CLICK_AIR:
+			case LEFT_CLICK_BLOCK:
+				double lx = vector.getX();
+				if(lx < 0) {
+					vector.setX(lx + 0.015);
+				} else if(0 < lx) {
+					vector.setX(lx - 0.015);
+				}
+
+				double ly = vector.getY();
+				if(ly < 0) {
+					vector.setY(ly + 0.015);
+				} else if(0 < ly) {
+					vector.setY(ly - 0.015);
+				}
+
+				double lz = vector.getZ();
+				if(lz < 0) {
+					vector.setZ(lz + 0.015);
+				} else if(0 < lz) {
+					vector.setZ(lz - 0.015);
+				}
+
+				ie.getPlayer().getVehicle().getWorld().playSound(ie.getPlayer().getVehicle().getLocation(), Sound.IRONGOLEM_HIT, 1, 0);
+				break;
+
+			// トロッコ乗車中に右クリック（加速）
+			case RIGHT_CLICK_AIR:
+			case RIGHT_CLICK_BLOCK:
+				double rx = vector.getX();
+				if(rx < 0) {
+					vector.setX(rx - 0.015);
+				} else if(0 < rx) {
+					vector.setX(rx + 0.015);
+				}
+
+				double ry = vector.getY();
+				if(ry < 0) {
+					vector.setY(ry - 0.015);
+				} else if(0 < ry) {
+					vector.setY(ry + 0.015);
+				}
+
+				double rz = vector.getZ();
+				if(rz < 0) {
+					vector.setZ(rz - 0.015);
+				} else if(0 < rz) {
+					vector.setZ(rz + 0.015);
+				}
+
+				ie.getPlayer().getVehicle().getWorld().playSound(ie.getPlayer().getVehicle().getLocation(), Sound.IRONGOLEM_HIT, 1, 0);
+				break;
+
+			default:
+				break;
+			}
+			ie.getPlayer().getVehicle().setVelocity(vector);
+			vector = null;
+		}
+	}
+}
