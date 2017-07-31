dc2b266a6824d00799c7fa5a32dfc6bb0e2119ea
Ryo Nakagome <nakagome@tento-net.com>
2017-07-31 16:01:00 +0900

    移動できました。
    Close #3

--- src/com/densyakun/ns/LoginMessenger.java
+++ src/com/densyakun/ns/LoginMessenger.java
@@ -1,7 +1,20 @@
 package com.densyakun.ns;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class LoginMessenger implements Listener {
-
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent je) {
+		Server server = Bukkit.getServer();
+		Player player = je.getPlayer();
+		je.getPlayer().sendMessage(ChatColor.AQUA + "ようこそ" + player.getDisplayName() + "さん！");
+		server.broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ChatColor.AQUA + "さんがオンラインになりました。");
+		server.broadcastMessage(ChatColor.GOLD + "みなさんで歓迎しましょう！");
+	}
 }
--- src/com/densyakun/ns/NSPLoader.java
+++ src/com/densyakun/ns/NSPLoader.java
@@ -1,36 +1,31 @@
 package com.densyakun.ns;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Server;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NSPLoader extends JavaPlugin implements Listener {
 
-	//初期動作
-	public void onEnable() {
+	//読み込み動作
+	public void onLoad() {
+		getLogger().info("プラグインを読み込み中です。");
+
+		//Load Other Classes
 		//This Event
 		getServer().getPluginManager().registerEvents(this, this);
 
 		//Vehicle Control
 		getServer().getPluginManager().registerEvents(new VehicleControl(), this);
+	}
+
+	//読み込み動作完了後の動作
+	public void onEnable() {
+		//LogMessage
 		getLogger().info("有効になりました");
 	}
 
+	//サーバ終了後の動作
 	public void onDisable() {
-
+		getLogger().info("プラグインが無効になりました。")
 	}
 
-
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent je) {
-		Server server = getServer();
-		Player player = je.getPlayer();
-		je.getPlayer().sendMessage(ChatColor.AQUA + "ようこそ" + player.getDisplayName() + "さん！");
-		server.broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ChatColor.AQUA + "さんがオンラインになりました。");
-		server.broadcastMessage(ChatColor.GOLD + "みなさんで歓迎しましょう！");
-	}
 }
