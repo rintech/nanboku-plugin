464288f251b964b1b638026ffa32efce2277a982
Ryo Nakagome <nakagome@tento-net.com>
2017-07-31 15:43:38 +0900

    ログインメッセージを表示する機能を追加

--- src/com/densyakun/ns/NSPLoader.java
+++ src/com/densyakun/ns/NSPLoader.java
@@ -1,6 +1,11 @@
 package com.densyakun.ns;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NSPLoader extends JavaPlugin implements Listener {
@@ -19,7 +24,7 @@ public class NSPLoader extends JavaPlugin implements Listener {
 
 	}
 
-	/*
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent je) {
 		Server server = getServer();
@@ -27,5 +32,5 @@ public class NSPLoader extends JavaPlugin implements Listener {
 		je.getPlayer().sendMessage(ChatColor.AQUA + "ようこそ" + player.getDisplayName() + "さん！");
 		server.broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ChatColor.AQUA + "さんがオンラインになりました。");
 		server.broadcastMessage(ChatColor.GOLD + "みなさんで歓迎しましょう！");
-	}*/
+	}
 }
